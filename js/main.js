356976d2cf16ba78fa9d597afb4cbebabe7018d6
benjamin <degroote.benjamin@gmail.com>
2017-06-19 13:47:36 +0200

    add article with description and img

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ var text = document.getElementsByClassName("alt");
 for (let i = 0 ; i < img.length ; i++) {
 img[i].addEventListener("mouseover", function() {
   var alt = img[i].alt;
-  text[i].textContent = alt;
+  text[i].textContent = "Le alt de l'image est " + alt;
 }
 );
 };
