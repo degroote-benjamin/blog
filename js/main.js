368ffe8eff0a4c773ea8d1154af11ffd1247d0d0
benjamin <degroote.benjamin@gmail.com>
2017-06-19 12:03:52 +0200

    add hover alt for all img in DOM

--- js/main.js
+++ js/main.js
@@ -25,3 +25,24 @@ function augmenter() {
 }
 
 setInterval(augmenter, 1000);
+
+
+// HOVER ALT
+
+var img = document.getElementsByTagName("img");
+var text = document.getElementsByClassName("alt");
+
+for (let i = 0 ; i < img.length ; i++) {
+img[i].addEventListener("mouseover", function() {
+  var alt = img[i].alt;
+  text[i].textContent = alt;
+}
+);
+};
+
+for (let i = 0 ; i < img.length ; i++) {
+img[i].addEventListener("mouseout", function() {
+text[i].textContent = "";
+}
+);
+};
